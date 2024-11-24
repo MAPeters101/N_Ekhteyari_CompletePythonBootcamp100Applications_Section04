cf1e0bcca688910d8085aa5c58f8b5b3a81d648e
Mark Peters <mapeters101@earthlink.net>
2024-11-24 18:25:45 -0500

    Lesson033:  Work on lesson.

---
+++ lesson033.py
@@ -0,0 +1,16 @@
+
+def greet(name):
+    print(f"Hello, {name}")
+
+greet(name='Alice')
+greet('John')
+print()
+
+def power(base, exponent=2):
+    result = base ** exponent
+    return result
+
+print(power(3))
+print(power(3, 3))
+
+
