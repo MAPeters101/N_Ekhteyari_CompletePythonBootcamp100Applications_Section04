30c9dddd7d81f9877eec7af4b257c3699f882e04
Mark Peters <mapeters101@earthlink.net>
2024-11-24 18:07:26 -0500

    Lesson028:  Work on lesson.

---
+++ lesson028.py
@@ -0,0 +1,24 @@
+
+age = 25
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+print()
+
+age = 17
+if age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+print()
+
+age = input("Enter your age: ")
+if int(age) >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")
+print()
+
+
+
